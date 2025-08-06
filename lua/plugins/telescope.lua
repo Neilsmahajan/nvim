8dabc6f2b10d508bc0daa923b72dda6e74398d0a
Neil Mahajan <neilsmahajan@gmail.com>
2025-08-06 08:15:14 -0700

    Added <leader>fa to find all files with telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,6 +6,7 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
   keys = {
     { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+    { "<leader>fa", "<cmd>Telescope find_files hidden=true no_ignore=true<cr>", desc = "Find all files" },
     { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
     { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
     { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help tags" },
@@ -15,6 +16,13 @@ return {
       defaults = {
         layout_config = {
           horizontal = { preview_width = 0.6 },
+        },
+        file_ignore_patterns = { "%.git/", "node_modules/", "%.cache/" },
+      },
+      pickers = {
+        find_files = {
+          hidden = true,
+          find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
         },
       },
     })
