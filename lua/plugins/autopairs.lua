3d2c55bad89c81c46957ff76119b3b2ebf52f804
Neil Mahajan <neilsmahajan@gmail.com>
2025-08-12 16:09:52 -0700

    Add nvim-autopairs plugin to cmp dependencies for enhanced completion

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,35 @@
+-- ~/.config/nvim/lua/plugins/autopairs.lua
+
+return {
+  "windwp/nvim-autopairs",
+  event = "InsertEnter",
+  config = function()
+    local autopairs = require("nvim-autopairs")
+    local cmp = require("cmp")
+    
+    autopairs.setup({
+      check_ts = true, -- Use treesitter for better context
+      ts_config = {
+        lua = { "string", "source" },
+        javascript = { "string", "template_string" },
+        java = false, -- Don't check treesitter on java
+      },
+      disable_filetype = { "TelescopePrompt", "spectre_panel" },
+      fast_wrap = {
+        map = "<M-e>",
+        chars = { "{", "[", "(", '"', "'" },
+        pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+        offset = 0,
+        end_key = "$",
+        keys = "qwertyuiopzxcvbnmasdfghjkl",
+        check_comma = true,
+        highlight = "PmenuSel",
+        highlight_grey = "LineNr",
+      },
+    })
+
+    -- Integration with nvim-cmp
+    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+  end,
+}
